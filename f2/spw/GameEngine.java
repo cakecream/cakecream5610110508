25563a637c11cc2d03259a89ae1570279e7fcf50
cakecream <khanomcake_cream@hotmail.com>
2016-04-09 16:32:44 +0700

    #01 สร้างหน้าต่าง GUI ขึ้นมา โดยมีชื่อเกมส์ชื่อว่า Alien Attack , กำหนดสีพื้นหลัง และ ขนาดของหน้าต่างเกมส์

---
+++ f2/spw/GameEngine.java
@@ -0,0 +1,37 @@
+package f2.spw;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.Timer;
+
+
+public class GameEngine implements KeyListener{
+	GamePanel gp;
+	
+	public GameEngine(GamePanel gp) {
+		this.gp = gp;
+				
+	}
+	
+	
+	@Override
+	public void keyPressed(KeyEvent e) {	
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		//do nothing
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//do nothing		
+	} 
+}
